9af9b8b7cd68c8bc6e451bc813ae017a0db668a1
surajkesri <77367577+surajkesri@users.noreply.github.com>
2025-10-31 04:37:37 +0530

    MVP 2.0 except story and nurch

---
+++ src/components/annoHero/AnnoAirHero.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./AnnoHero.css";
+
+
+
+const AnnoAirHero: React.FC = () => {
+  
+
+  const handlePreBookClick = () => {
+    window.open("https://forms.gle/MWggLGwfgKCBbJpP6", "_blank");
+  };
+
+  
+  return (
+    <section className="anno-hero">
+      <img src='/ANNO Ren 1 1.png' alt="Anno Pod" className="anno-image" />
+      <div className="anno-overlay">
+        
+        <img className="anno-logo" src='/anno-logo.png' alt="anno Balcony"  />
+        <p className="anno-subtitle">
+          Grow 44 Plants  <br />
+          Footprint: 80cm x43cm (2'7" x 1'5") <br />
+          Height: 160cm (5'3") <br /><br />
+          Integrated with Special HEPA filters <br />
+          to actively purify your indoor air.
+        </p>
+        <h3>OFFER PRICE: INR 54,999</h3>
+        <div className="anno-buttons">
+          <button className="anno-btn install" onClick={handlePreBookClick}>Pre-Book</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AnnoAirHero;
